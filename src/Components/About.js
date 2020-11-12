4983bbbc2e106663cb41e0e4cd3335bcc4bfa7d4
Justin Langlinais <justinclanglinais@gmail.com>
2020-11-12 10:44:27 -0800

    feat(About): cleanup props variable assignments

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,18 +3,21 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {
 
-    if(this.props.data){
-      var name = this.props.data.name;
-      var profilepic= "images/"+this.props.data.image;
-      var bio = this.props.data.bio;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
-      var email = this.props.data.email;
-      var resumeDownload = this.props.data.resumedownload;
-    }
+   //  if(this.props.data){
+   //    var name = this.props.data.name;
+   //    var profilepic= "images/"+this.props.data.image;
+   //    var bio = this.props.data.bio;
+   //    var street = this.props.data.address.street;
+   //    var city = this.props.data.address.city;
+   //    var state = this.props.data.address.state;
+   //    var zip = this.props.data.address.zip;
+   //    var phone= this.props.data.phone;
+   //    var email = this.props.data.email;
+   //    var resumeDownload = this.props.data.resumedownload;
+   //  }
+    this.props.data ?
+      let { name, profilepic, bio, street, city, state, zip, phone, email, resumeDownload } = this.props.data
+      : null
 
     return (
       <section id="about">
